33c27fab64d2ca1c88f6406240d0ee41f798ea70
SimonAtoshvili <simoni.atoshvili@iset.ge>
2023-04-11 21:35:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,57 +3,39 @@ const header = document.querySelector("header");
 const headingTop = document.querySelector("h1");
 const mainSection = document.querySelector(".section_2");
 const mainEl = document.querySelector("main");
+const card = document.querySelector(".card")
 
 const func = async () => {
     const response = await fetch("https://restcountries.com/v3.1/all");
     const data = await response.json();
 
-    for (d = 0; d < data.length; d++) {
-        const div = document.createElement("div");
-        div.classList.add("card")
+    for (i = 0; i < data.length; i++) {
+        const newCard = document.createElement("div");
+        newCard.classList.add("new_card");
+        newCard.innerHTML = card.innerHTML;
 
-        const img = document.createElement("img");
-        const imgSrc = data[d]["flags"]["png"];
-        img.src = imgSrc;
+        mainSection.appendChild(newCard);
 
-        const headingSec = document.createElement("h2");
-        const name = data[d]["name"]["common"];
-        headingSec.innerText = name;
 
-        const population = document.createElement("p");
-        const boldPop = document.createElement("span");
-        boldPop.classList.add("bold_word")
-        const valuePop = document.createElement("span");
-        boldPop.innerText = "Population: ";
-        valuePop.innerText = data[d]["population"];
-        population.appendChild(boldPop);
-        population.appendChild(valuePop);
+        const flagsList = document.querySelectorAll("img");
+        const flag = flagsList[flagsList.length - 1];
+        flag.src = data[i]["flags"]["png"];
 
-        const region = document.createElement("p");
-        const boldReg = document.createElement("span");
-        boldReg.classList.add("bold_word")
-        const valueReg = document.createElement("span");
-        boldReg.innerText = "Region: ";
-        valueReg.innerText = data[d]["region"];
-        region.appendChild(boldReg);
-        region.appendChild(valueReg);
+        const namesList = document.querySelectorAll("h2");
+        const countryName = namesList[namesList.length - 1];
+        countryName.innerText = data[i]["name"]["common"];
 
-        const capitalPar = document.createElement("p");
-        const boldCap = document.createElement("span");
-        boldCap.classList.add("bold_word")
-        const valueCap = document.createElement("span");
-        boldCap.innerText = "Capital: ";
-        valueCap.innerText = data[d]["capital"] ? data[d].capital[0] : "NA";
-        capitalPar.appendChild(boldCap);
-        capitalPar.appendChild(valueCap);
+        const popList = document.querySelectorAll(".pop_value");
+        const pop = popList[popList.length - 1];
+        pop.innerText = data[i]["population"];
 
-        div.appendChild(img);
-        div.appendChild(headingSec);
-        div.appendChild(population);
-        div.appendChild(region);
-        div.appendChild(capitalPar);
+        const regList = document.querySelectorAll(".reg_value");
+        const reg = regList[regList.length - 1];
+        reg.innerText = data[i]["region"];
 
-        mainSection.appendChild(div);
+        const capList = document.querySelectorAll(".cap_value");
+        const cap = capList[capList.length - 1];
+        cap.innerText = data[i]["capital"] ? data[i]["capital"][0] : "NA";
     }
 
     mode.addEventListener("click", function () {
@@ -71,13 +53,15 @@ const func = async () => {
         const input = document.querySelector("input");
         select.classList.toggle("select_light");
         input.classList.toggle("input_light");
-        const cardsList = document.querySelectorAll(".card");
+        const cardsList = document.querySelectorAll(".new_card");
         const pList = document.querySelectorAll("p");
         const nameList = document.querySelectorAll("h2");
         for (let i = 0; i < pList.length; i++) {
-            cardsList[i].classList.toggle("card_light");
-            nameList[i].classList.toggle("h2_light");
             pList[i].classList.toggle("p_light");
+        }
+        for (let i = 0; i < cardsList.length; i++) {
+            cardsList[i].classList.toggle("new_card_light");
+            nameList[i+1].classList.toggle("h2_light");
         }
     });
 }
