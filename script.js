e6f101e68d26713bcfadcc4d529caa8b4a768fda
SimonAtoshvili <simoni.atoshvili@iset.ge>
2023-04-11 19:04:35 +0400

    Selected some elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const mode = document.querySelector(".mode");
+const header = document.querySelector("header");
+const headingTop = document.querySelector("h1");
+const mainSection = document.querySelector(".section_2");
+const mainEl = document.querySelector("main");
